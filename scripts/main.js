2d0ce1b8d331f7a16172e3fa149e8ef65d1c3c01
Matt Claghorn <claggy0@yahoo.com>
2018-02-23 17:13:40 -0500

    add divs and DOM methods for showing score

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,11 @@
-
 let myScore = 0;
 let compScore = 0;
 let round = 1;
 
 const buttons = document.querySelectorAll('button');
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
-
+const playerScore = document.querySelector('#playerScore');
+const computorScore = document.querySelector('#computerScore');
+const gameWinner = document.querySelector('#winner');
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -16,94 +14,72 @@ buttons.forEach((button) => {
   button.addEventListener('click', game);
 });
 
-
-function playGame() {
+function resetGame() {
   myScore = 0;
   compScore = 0;
   round = 1;
-  game();
 }
 
 function game() {
   let computerSelection = computerPlay();
 
-  if (round === 1 || round === 2 || round === 3 || round === 4) {
-    roundScore(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    
-    if (round >= 3 && (myScore === 3 || compScore === 3)) {
-      winner();
-    } else {
-      round++;
-    }
+  updateScore(playerSelection, computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
+  round++;
 
-  } else if (round >= 5) {
-    roundScore(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-
-    if (myScore === compScore) {
-      console.log(`You\'re still tied after ${round} rounds- time for a tiebreaker!`);
-      round++;
-      game();
-    } else {
-      winner();
-    }
+  if (myScore === 5 || compScore === 5) {
+    winner();
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  let currentScore = `Current score is You: ${myScore} Computer: ${compScore}`;
 
   switch (true) {
     case playerSelection === 'rock' && computerSelection === 'paper':
-      return `You lose round ${round}! Paper beats rock. ${currentScore}`;
+      return `You lose round ${round}! Paper beats rock.`;
       break;
 
     case playerSelection === 'rock' && computerSelection === 'rock':
-      return `Round ${round} is a tie! You both chose ${playerSelection}. ${currentScore}`;
+      return `Round ${round} is a tie! You both chose ${playerSelection}.`;
       break;
 
     case playerSelection === 'rock' && computerSelection === 'scissors':
-      return `You win round ${round}! Rock beats scissors. ${currentScore}`;
+      return `You win round ${round}! Rock beats scissors.`;
       break;
 
     case playerSelection === 'paper' && computerSelection === 'paper':
-      return `Round ${round} is a tie! You both chose ${playerSelection}. ${currentScore}`;
+      return `Round ${round} is a tie! You both chose ${playerSelection}.`;
       break;
 
     case playerSelection === 'paper' && computerSelection === 'rock':
-      return `You win round ${round}! Paper beats rock. ${currentScore}`;
+      return `You win round ${round}! Paper beats rock.`;
       break;
 
     case playerSelection === 'paper' && computerSelection === 'scissors':
-      return `You lose round ${round}! Scissors beats paper. ${currentScore}`;
+      return `You lose round ${round}! Scissors beats paper.`;
       break;
 
     case playerSelection === 'scissors' && computerSelection === 'scissors':
-      return `Round ${round} is a tie! You both chose ${playerSelection}. ${currentScore}`;
+      return `Round ${round} is a tie! You both chose ${playerSelection}.`;
       break;
 
     case playerSelection === 'scissors' && computerSelection === 'rock':
-      return `You lose round ${round}! Rock beats scissors. ${currentScore}`;
+      return `You lose round ${round}! Rock beats scissors.`;
       break;
 
     case playerSelection === 'scissors' && computerSelection === 'paper':
-      return `You win round ${round}! Scissors beats paper. ${currentScore}`;
+      return `You win round ${round}! Scissors beats paper.`;
       break;
-
-    case playerSelection === undefined:  
-      break;
-
-    default:
-      return 'Invalid input. Choose Rock, Paper, or Scissors.'
   }
 }
 
-function roundScore(playerSelection, computerSelection) {
+function updateScore(playerSelection, computerSelection) {
   if (playRound(playerSelection, computerSelection).includes("win")) {
     myScore++;
+    playerScore.textContent = myScore;
   } else if (playRound(playerSelection, computerSelection).includes("lose")) {
     compScore++;
+    computerScore.textContent = compScore;
   } else if (playRound(playerSelection, computerSelection).includes("tie")) {
     myScore + 0;
     compScore + 0;
@@ -132,9 +108,4 @@ function computerPlay() {
   } else {
     return 'scissors';
   }
-}
-
-
-function playerChoice() {
-
 }
